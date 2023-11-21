bc8b3721bdb84f223f947010120f458fe0efebfb
SHADIMK7 <muhammedshadi.mk7@gmail.com>
2023-11-21 18:47:44 +0530

    commit-1

---
+++ todoapp/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from todoapp import views
+
+urlpatterns = [
+    path('',views.add,name='add'),
+    path('delete/<int:taskid>/',views.delete, name='delete'),
+    path('update/<int:id>/',views.update, name='update'),
+    path('cbvhome/',views.TaskListview.as_view(), name = 'cbvhome'),
+    path('cbvdetail/<int:pk>/',views.TaskDetailview.as_view(), name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.TaskUpdateview.as_view(), name='cbvupdate'),
+    path('cbvdelete/<int:pk>/', views.TaskDeleteview.as_view(), name='cbvdelete')
+    # path('details', views.details, name='details')
+]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL,
+                                       document_root=settings.STATIC_ROOT)
